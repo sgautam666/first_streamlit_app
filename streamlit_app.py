9569797bf940495e0aa20e7a8a4b710103db44e7
Suman Gautam <smngeo@gmail.com>
2022-04-15 10:51:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.text('Avocado Toast')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fuit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Fruit pick lists
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
